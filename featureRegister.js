69cff9f846b0d8d57cd45d9665960918aafdf681
rezanasu <rezanasution82@gmail.com>
2023-06-28 20:18:52 +0700

    fitur register done

---
+++ featureRegister.js
@@ -0,0 +1,7 @@
+function register(email, password, age) {
+
+    console.log("Registered successfully")
+    console.log(`Email: ${email}, Password: ${password}, Age: ${age}`)
+}
+
+register("user@mail.com", "user", 15)
